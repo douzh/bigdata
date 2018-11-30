f6670dbce9f04a178b94936aa52802a2f842031e
douzh <douzihui@9fbank.com.cn>
2018-11-30 16:53:56 +0800

    修改

--- spark/sparkdemo/src/test/scala/com/iteedu/demo/spark/CsvFileDemo.scala
+++ spark/sparkdemo/src/test/scala/com/iteedu/demo/spark/CsvFileDemo.scala
@@ -19,14 +19,20 @@ object CsvFileDemo {
     val conf = new SparkConf().setAppName("spark init test");
     conf.setMaster("local");
     val spark = SparkSession.builder.config(conf).getOrCreate();
-    var csv = spark.read.format("csv").option("header", "false").option("delimiter", "\001").load("D:/data/inv_contract.csv");
-    csv.createOrReplaceTempView("proinfo");
-    val lstR=csv.rdd.map(r=>r.mkString("\001")).collect()
-    val writer = new PrintWriter(new File("D:/test.csv"))
-    writer.println(csv.columns.mkString("\001"))
-    lstR.foreach(r=>writer.println(r))
-    writer.flush()
-    writer.close()
+    var csv = spark.read.format("csv").option("header", "false").option("delimiter", "|").load("D:/data/advertest/adver_info.csv");
+    csv.createOrReplaceTempView("proinfo")
+    spark.sql("select * from proinfo").show()
+    spark.sql("select * from proinfo where _c1 not rlike '1|0'").show()
+    spark.sql("select * from proinfo where _c1 is null").show()
+    
+    
+    //    csv.createOrReplaceTempView("proinfo");
+//    val lstR=csv.rdd.map(r=>r.mkString("\001")).collect()
+//    val writer = new PrintWriter(new File("D:/test.csv"))
+//    writer.println(csv.columns.mkString("\001"))
+//    lstR.foreach(r=>writer.println(r))
+//    writer.flush()
+//    writer.close()
     
   }
 }
